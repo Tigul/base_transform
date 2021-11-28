8840c843cee9a59caf9856dd3a2486808ba36fe6
Jonas Dech <j.dech@gmx.net>
2021-11-28 15:06:30 +0100

    docu

--- src/transform_base.py
+++ src/transform_base.py
@@ -6,12 +6,23 @@ from geometry_msgs.msg import PoseWithCovariance, TransformStamped, Vector3, Poi
 from sensor_msgs.msg import JointState
 import time
 
+# The pose published by openvr_ros
 global tf_pub
+# The tf_listener instance
 global tf_listener
-
+# The joint pose of the head_yaw_joint of Pepper
 global head_yaw_pose
 
 def tf_callback(msg):
+    """
+    The callback method for Messages published to the TF topic.
+    The Method applies a constant transform which transforms the position of the
+    tracker to point in the direction of the robot.
+    Afterwards the current rotation of the robot head will be applied, to compensate
+    for head movements and let the pose still point in the direction of the robot
+    despite the head being turned.
+    :param msg: The current message of the TF topic
+    """
     global tf_pub
     global tf_listener
     global head_yaw_pose
@@ -26,10 +37,13 @@ def tf_callback(msg):
         msg_stamp.transform.translation.y = tra[1]
         msg_stamp.transform.translation.z = 0
 
-        #rot = quaternioin_to_list(msg.transforms[0].transform.rotation)
+        # Create quaternion from head joint position
         head_quanternion = list(tf.transformations.quaternion_from_euler(0, 0, head_yaw_pose, axes="sxyz"))
+        # concatenate current rotation of the tracker with the correction from head joint
         correct_head = t.concatenate_matrices(t.quaternion_matrix(rot), t.quaternion_matrix(head_quanternion))
+        # concatenate the corrected head matrix with the constatnt quaternion to let the tracker point in same direction as the robot
         new_q = t.concatenate_matrices(correct_head, t.quaternion_matrix(quatenrnion))
+        # Convert finished matrix back to quaternion
         inv = t.quaternion_from_matrix(new_q)
         msg_stamp.transform.rotation.x = inv[0]
         msg_stamp.transform.rotation.y = inv[1]
